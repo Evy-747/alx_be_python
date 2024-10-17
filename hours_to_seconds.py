b8a17a3778d0b315037de4f3b23760f9bf62d9f7
Evy-747 <jeronoevy@gmail.com>
2024-10-17 11:29:06 +0300

    add hours_to_seconds.py to calculate number of seconds

---
+++ hours_to_seconds.py
@@ -0,0 +1,10 @@
+#define number of hours
+hours = 2 
+
+#calculate number of seconds in the given hours
+seconds = (hours * 60 * 60)
+
+#print the result
+print ( f" {hours} hr(s) is {seconds} seconds ")
+
+
